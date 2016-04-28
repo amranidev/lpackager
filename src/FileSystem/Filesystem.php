2acd7575543236274b889215ef461a875a5d8084
Amrani <amranidev@gmail.com>
2016-04-28 11:41:33 +0100

    update fileSystem

--- src/FileSystem/Filesystem.php
+++ src/FileSystem/Filesystem.php
@@ -4,4 +4,50 @@ namespace Armandiev\Lpackager\FileSystem;
 
 class Filesystem extends \Exception
 {
+    /**
+     * Make a file
+     *
+     * @param $file
+     * @param $content
+     * @throws FileAlreadyExists
+     * @return int
+     */
+    public function make($file, $content)
+    {
+        if ($this->exists($file)) {
+            throw new \Exception("FileAlreadyExists");
+        }
+
+        return file_put_contents($file, $content);
+    }
+
+    /**
+     * Determine if file exists
+     *
+     * @param $file
+     * @return bool
+     */
+    public function exists($file)
+    {
+        return file_exists($file);
+    }
+
+    /**
+     * make Directory
+     */
+    public function makeDir($path)
+    {
+        if (is_dir($path)) {
+            throw new \Exception("FileAlreadyExists");
+        }
+        mkdir($path, 0777, true);
+    }
+
+    /**
+     * File Append
+     */
+    public function append($path, $content)
+    {
+        return file_put_contents($path, $content, FILE_APPEND | LOCK_EX);
+    }
 }
