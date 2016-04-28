7cff3bc161bbbe89ad358bdb0533b86effa601ba
Amrani <amranidev@gmail.com>
2016-04-28 11:41:52 +0100

    update path

--- src/FileSystem/Path.php
+++ src/FileSystem/Path.php
@@ -2,28 +2,87 @@
 
 namespace Amranidev\lpackager\FileSystem;
 
-class Path 
+class Path
 {
-	/**
-	 * package name
-	 * 
-	 * @var package
-	 */ 
-	private $package;
-	private $path;
-	private $namespace;
+    /**
+     * package name
+     * 
+     * @var package
+     */
+    private $package;
 
-	public function __construct($package, $path, $namespace)
-	{
-		$this->package = $package;
-		$this->path = $path;
-		$this->namespace = $namespace;
-	}
+    /**
+     * package path
+     * 
+     * @var path
+     */ 
+    private $path;
 
-	public function root()
-	{
-		base_path() .'/'. $this->path .'/'.$this->package;
-	}
+    /**
+     * Create new Path instance
+     * 
+     * @param String $package;
+     * @param String $path
+     */ 
+    public function __construct($package, $path)
+    {
+        $this->package = $package;
 
+        $this->path = $path;
+    }
 
+    /**
+     * get package root path
+     * 
+     * @return Mixed
+     */ 
+    public function root()
+    {
+        return base_path() . '/' . $this->path . '/' . $this->package;
+    }
+
+    public function resources()
+    {
+        return $this->root() . '/resources/views';
+    }
+
+    public function database()
+    {
+        return $this->root() . '/database/migrations';
+    }
+
+    public function config()
+    {
+        return $this->root() . '/config';
+    }
+
+    public function src()
+    {
+        return $this->root() . '/src/Http/Controllers';
+    }
+
+    public function routes()
+    {
+        return $this->root() . '/src/Http/routes.php';
+    }
+
+    public function controller()
+    {
+        return $this->root() . '/src/Http/Controllers/WelcomeController.php';
+    }
+
+    public function serviceProvider()
+    {
+        return $this->root() . '/src/' . $this->package . 'ServiceProvider.php';
+    }
+
+    public function view()
+    {
+        return $this->root() . '/resources/views/welcome.blade.php';
+    }
+
+    public function configFile()
+    {
+        return $this->config() . '/config.php';
+    }
 }
