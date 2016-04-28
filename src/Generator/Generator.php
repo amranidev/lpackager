c31afb67e72e7d49a6cf2e55e85e138ce1500403
Amrani <amranidev@gmail.com>
2016-04-28 11:42:13 +0100

    update generator

--- src/Generator/Generator.php
+++ src/Generator/Generator.php
@@ -7,11 +7,36 @@ use Armandiev\Lpackager\FileSystem\Filesystem;
 
 class Generator extends Filesystem
 {
-	private $path;
+    private $path;
 
-	public function __construct(Path $path)
-	{
-		$this->path = $path;
-	}
+    public function __construct(Path $path, $namespace)
+    {
+        $this->path = $path;
+    }
+
+    public function root()
+    {
+        $this->makeDir($this->path->root());
+    }
+
+    public function resources()
+    {
+        $this->makeDir($this->path->resources());
+    }
+
+    public function database()
+    {
+        $this->makeDir($this->path->database());
+    }
+
+    public function config()
+    {
+        $this->makeDir($this->path->config());
+    }
+
+    public function src()
+    {
+        $this->makeDir($this->path->src());
+    }
 
 }
