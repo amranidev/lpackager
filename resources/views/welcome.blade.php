645b3539f8e7d3c140720c1f1c1435efe2fd85ad
Amrani <amranidev@gmail.com>
2016-05-02 23:03:53 +0100

    update view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,41 @@
-@@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@{{$package}}</title>
 
-@@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-                <div class="panel-body">
-                    <h1>@{{$package}}</h1>
-                </div>
+        <style>
+            html, body {
+                height: 100%;
+            }
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">@{{$package}}</div>
             </div>
         </div>
-    </div>
-</div>
-@@endsection
+    </body>
+</html>
