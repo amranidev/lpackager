43fa21ec0845cf5950d2f53a17394e6479f596e8
Amrani <amranidev@gmail.com>
2016-05-18 13:24:26 +0100

    add comments to controller generator command

--- resources/views/GeneratorCommands/controller.blade.php
+++ resources/views/GeneratorCommands/controller.blade.php
@@ -4,6 +4,11 @@ use {{$package}}AppController as Controller;
 
 class {{$className}} extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
 	public function index()
 	{
 		//
