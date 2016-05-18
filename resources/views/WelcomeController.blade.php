f5878e960d1d2533a87f68457517d26a98f8f907
Amrani <amranidev@gmail.com>
2016-05-18 13:26:08 +0100

    add comments to WelcomeController

--- resources/views/WelcomeController.blade.php
+++ resources/views/WelcomeController.blade.php
@@ -6,12 +6,15 @@ use {{$package}}AppController as Controller;
 
 class WelcomeController extends Controller
 {
-
+	/**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
 	public function index()
 	{
 	    $package = "{{$package}}";
 
 		return view('{{$package}}::welcome' , compact('package'));
 	}
-
 }
